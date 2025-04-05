a25677e9603a266d474fec67597afe082b02443f
yelena0000 <kirillnek07081999@gmail.com>
2025-04-05 21:41:36 +0300

    add VK bot

---
+++ vk_bot.py
@@ -0,0 +1,27 @@
+import vk_api
+from environs import Env
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+
+def main() -> None:
+    env = Env()
+    env.read_env()
+
+    vk_group_token = env.str('VK_GROUP_TOKEN')
+
+    vk_session = vk_api.VkApi(token=vk_group_token)
+
+    longpoll = VkLongPoll(vk_session)
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW:
+            print('Новое сообщение:')
+            if event.to_me:
+                print('Для меня от: ', event.user_id)
+            else:
+                print('От меня для: ', event.user_id)
+            print('Текст:', event.text)
+
+
+if __name__ == '__main__':
+    main()
